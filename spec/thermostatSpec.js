96a7a64b14216c579b0ed1f4c90d68625a9814bc
Gabby Sanghera <gabby.sanghera@gmail.com>
2019-12-10 14:27:42 +0000

    added maximum limit for power saving mode on and off. added values at top of src, tests all pass, test has for loop that factors in default temperature add 6

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -45,4 +45,13 @@ beforeEach(function() {
     expect(thermostat.isPowerSavingModeOn()).toBe(true);
   });
 
+  describe('when power saving mode is on', function () {
+    it ('has a maximum temperature of 25 degrees', function () {
+      for (let i = 0; i < 6; i++) {
+        thermostat.up();
+      }
+        expect(thermostat.getCurrentTemperature()).toEqual(25);
+    });
+  });
+
 });
